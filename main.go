4711e432a078f43e16d8ae0423d2d19f0b7bbace
Ishu <ishubharatbhaipad@loyalistcollege.com>
2024-07-11 18:56:12 -0400

    Update main.go
    part 5 ishu

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main()
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", home)
+    http.HandleFunc("/about", about)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to the API Server!")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "This is a simple API built with Go.")
+}
