775b6a8503aa187ce30645dc589b77aad7d9fb90
keyursavaliya02 <116817178+keyursavaliya02@users.noreply.github.com>
2024-07-11 18:51:30 -0400

    Update main.go
    part 4 Keyur

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the API Server!")
+}
